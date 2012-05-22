03dbe91b3afcd9cb73ffe9792a0999024ae4164e
Greg Clark <gclark916@ucla.edu>
2012-05-21 21:15:17 -0700

    added getPacketSize function to UniChannel. Assumes tuples and channels (not necessarily elements) start on byte boundaries.

--- uniOpenNI/src/UniChannel.java
+++ uniOpenNI/src/UniChannel.java
@@ -22,6 +22,29 @@ public class UniChannel {
 		return true;
 	}
 	
+	long getPacketSize() 
+	{
+		// Construct size (in bits) of a tuple
+		// TODO: assumes that tuples and channels are byte-aligned, not necessarily elements
+		UniElementDescriptor descriptors[] = header.getElementDescriptors();
+		long tupleSizeInBits = 0;
+		for (int i = 0; i < descriptors.length; ++i) 
+		{
+			int elementSizeInBits = descriptors[i].getSize() * (descriptors[i].isSizedInBytes()?1:0 << 3);
+			tupleSizeInBits += elementSizeInBits;
+		}
+		
+		// Construct buffer size (in bytes)
+		long channelSizeInBits = tupleSizeInBits * header.getNumberTuples();
+		long channelSize = channelSizeInBits >> 3;
+		channelSize += ((channelSizeInBits & 7) > 0 ? 1 : 0); // Add an extra byte if data doesn't end on byte boundary
+		channelSize += 18; 	// Add size of timestamp, frequency, elementsPerTuple
+		channelSize += descriptors.length; // Add size of element descriptors
+		channelSize += ((header.getName().length() + 1) << 1); // Add size of name
+		
+		return channelSize;
+	}
+	
 	ByteBuffer getData() { return data; }
 	UniChannelHeader getHeader() { return header; }
 }
