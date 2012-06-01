61b21894e8426c2d4c72bcc6370f6f7d3b4578e0
Greg Clark <gclark916@ucla.edu>
2012-05-31 19:32:53 -0700

    Moved Unitype enum to its own java file and added to repo

---
+++ uniOpenNI/src/UniType.java
@@ -0,0 +1,16 @@
+/**
+ * Enum for element types.
+ * @author Greg Clark
+ *
+ */
+enum UniType 
+{
+	int8,
+	int16,
+	int32,
+	int64,
+	float32,
+	float64,
+	int_nonprimitive,
+	float_nonprimitive;
+}
